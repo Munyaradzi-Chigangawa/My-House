94998385764cd8c4ee8d308fbdf25882f2268b33
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-22 01:22:03 +0200

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My House',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: AuthScreen(),
