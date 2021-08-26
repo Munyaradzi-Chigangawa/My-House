ad2da96b918595aaf55f0fb90967e2fa941a0def
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-27 00:55:32 +0200

    added signout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_house/main.dart';
+import 'package:my_house/models/userModel.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({Key? key, required this.user}) : super(key: key);
+ final UserModel user;
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +40,15 @@ class _HomeState extends State<Home> {
         title: Text('Home'),
         actions: [
           IconButton(
-              onPressed: () {}, icon: Icon(Icons.power_settings_new_outlined))
+              onPressed: () async{
+                FirebaseAuth.instance.signOut().then((value){
+                  Navigator.pushReplacement(context, 
+                  MaterialPageRoute(
+                    builder: (context) => AuthenticateUser(),
+                    ));
+                });
+              }, 
+              icon: Icon(Icons.power_settings_new_outlined))
         ],
       ),
       body: Container(),
