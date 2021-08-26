cc8f54151f684db51f2a9bd593b20dc2b0f012f5
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-27 00:08:26 +0200

    added future builder

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,20 @@ class AuthenticateUser extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return FutureBuilder(
+      future: chechCurrentUser(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting){
+          return Container(
+            color: Colors.blue,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          return snapshot.data;
+        }
+      },
     );
   }
 }
