943a020192a2ced8ec12b62e41b8e9f233cf451a
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-27 13:52:24 +0200

    initial commit

---
+++ lib/screens/notInvited.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Notinvited extends StatelessWidget {
+  const Notinvited({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.redAccent,
+        child: Center(
+          
+        ),
+      )
+    );
+  }
+}
