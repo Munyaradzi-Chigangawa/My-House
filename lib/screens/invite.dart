211b73bb8b17fa289e1398eb8bf6dc3b58d23003
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-27 02:09:26 +0200

    adding textfield and invite button

--- lib/screens/invite.dart
+++ lib/screens/invite.dart
@@ -9,6 +9,7 @@ class Invite extends StatefulWidget {
 }
 
 class _InviteState extends State<Invite> {
+  final TextEditingController inviteController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,9 +39,35 @@ class _InviteState extends State<Invite> {
               ),
             ),
             SizedBox(height: 50,),
-            TextField(
-              
-            )
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: TextField(
+                controller: inviteController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Friend's mobile number with country code.",
+                  hintText: "(eg: +26377*******)"
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(onPressed: () {
+
+              }, 
+              child: Text('Send Invite', 
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                ),)),
+            ),
           ],
         ),),
     );
