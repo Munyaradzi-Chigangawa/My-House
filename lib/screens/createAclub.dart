bece596e33fb9ecc472ba00e43159ba75276f103
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-27 16:53:54 +0200

    create club page

---
+++ lib/screens/createAclub.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateAClub extends StatefulWidget {
+  const CreateAClub({ Key? key }) : super(key: key);
+
+  @override
+  _CreateAClubState createState() => _CreateAClubState();
+}
+
+class _CreateAClubState extends State<CreateAClub> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
