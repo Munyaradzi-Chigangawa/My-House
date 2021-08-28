40252eda00e733f3b4639c60103f50e034eb709c
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-28 08:04:12 +0200

    creating a form

--- lib/screens/createAclub.dart
+++ lib/screens/createAclub.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:my_house/models/userModel.dart';
 
 class CreateAClub extends StatefulWidget {
-  const CreateAClub({ Key? key }) : super(key: key);
+  const CreateAClub({ Key? key, required this.user }) : super(key: key);
+  final UserModel user;
 
   @override
   _CreateAClubState createState() => _CreateAClubState();
 }
 
 class _CreateAClubState extends State<CreateAClub> {
+
+  final GlobalKey <FormState> _formKey = GlobalKey <FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Create your Club'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                
+              ],
+            )
+          ),
+        )
+      ),
     );
   }
 }
