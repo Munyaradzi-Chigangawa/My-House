1b75d16e7599d73bd6dc2faedd036bc5970fd253
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-31 17:40:46 +0200

    profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:my_house/models/userModel.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({ Key? key }) : super(key: key);
-
+  const Profile({ Key? key, required this.user }) : super(key: key);
+  final UserModel user;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+        backgroundColor: Color(0xfff1efe5),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
     );
   }
 }
