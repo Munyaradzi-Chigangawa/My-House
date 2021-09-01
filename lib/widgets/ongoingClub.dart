ac2cffd73848e509333857697747342f844ffdb3
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-09-01 16:21:54 +0200

    added divider to ongoing clubs

--- lib/widgets/ongoingClub.dart
+++ lib/widgets/ongoingClub.dart
@@ -29,7 +29,8 @@ class OngoingClub extends StatelessWidget {
                   overflow: TextOverflow.ellipsis,
                   ))
             ],
-          )
+          ),
+          Divider()
         ],
       ),
     );
