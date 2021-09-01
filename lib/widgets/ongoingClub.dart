c84f2cc562a9f0b76772e4eab701d683d0560e87
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-09-01 15:52:17 +0200

    ongoing club

--- lib/widgets/ongoingClub.dart
+++ lib/widgets/ongoingClub.dart
@@ -6,7 +6,23 @@ class OngoingClub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color(0xfff1efe5)
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'time', 
+                style: TextStyle(
+                  color: Colors.black),
+                  )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
