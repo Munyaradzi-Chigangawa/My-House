83c5af9586cf0c99d09cb7a4cc3b0655cbf76c99
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-09-07 18:50:39 +0200

    adding club screen

---
+++ lib/screens/clubScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ClubScreen extends StatefulWidget {
+  const ClubScreen({ Key? key }) : super(key: key);
+
+  @override
+  _ClubScreenState createState() => _ClubScreenState();
+}
+
+class _ClubScreenState extends State<ClubScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
